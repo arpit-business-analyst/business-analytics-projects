2414a695d0540c0df2286d6206ee7ab4c69009d8
arpit-business-analyst <arpitagarwal2702@gmail.com>
2026-01-21 20:24:09 +0530

    world data analysis project structure

---
+++ SQL/world_data_analysis_project.sql
@@ -0,0 +1,21 @@
+-- =====================================================
+-- Project: World Data Analysis
+-- Author: Arpit Agarwal
+-- Description: SQL analysis on world country data
+-- Dataset: world table
+-- =====================================================
+
+-- BUSINESS QUESTION 1:
+-- List all countries with population greater than 100 million
+
+-- BUSINESS QUESTION 2:
+-- Find the total population by continent
+
+-- BUSINESS QUESTION 3:
+-- Identify the top 5 most populated countries
+
+-- BUSINESS QUESTION 4:
+-- Find countries whose population is above the world average
+
+-- BUSINESS QUESTION 5:
+-- List continents having more than 10 countries
