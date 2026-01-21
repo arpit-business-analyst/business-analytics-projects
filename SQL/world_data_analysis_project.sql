5c9c5d62a006fbac1bd047ee62d496e81d68e869
arpit-business-analyst <arpitagarwal2702@gmail.com>
2026-01-21 20:37:16 +0530

    Solve business questions 1 and 2

--- SQL/world_data_analysis_project.sql
+++ SQL/world_data_analysis_project.sql
@@ -1,15 +1,25 @@
 -- =====================================================
 -- Project: World Data Analysis
--- Author: Arpit Agarwal
+-- Author: Arpit Agrawal
 -- Description: SQL analysis on world country data
 -- Dataset: world table
 -- =====================================================
 
 -- BUSINESS QUESTION 1:
 -- List all countries with population greater than 100 million
+-- ANSWER 
+SELECT name, population
+FROM world
+WHERE population > 100000000
+ORDER BY population DESC;
 
 -- BUSINESS QUESTION 2:
 -- Find the total population by continent
+-- ANSWER
+SELECT continent, SUM(population) AS Total_Population
+FROM world
+GROUP BY continent
+ORDER BY Total_Population DDESC;
 
 -- BUSINESS QUESTION 3:
 -- Identify the top 5 most populated countries
